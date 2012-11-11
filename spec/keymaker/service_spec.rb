aca37cf6fb71eb2a5ea993bec1268e655ff81c72
Rogelio J. Samour <rogelio@therubymug.com>
2012-11-11 00:37:23 -0500

    Consolidate Keymaker::Service spec

--- spec/keymaker/service_spec.rb
+++ spec/keymaker/service_spec.rb
@@ -28,4 +28,19 @@ describe Keymaker::Service do
     end
   end
 
+  context "with credentials provided" do
+    let(:options) { { username: "jconnor", password: "easymoney" } }
+    let(:config) do
+      Keymaker::Configuration.new(options)
+    end
+    let(:service) { Keymaker::Service.new(config) }
+
+    describe "#connection" do
+      it "includes the username and password" do
+        Faraday.should_receive(:new).with(url: config.connection_service_root_url)
+        service.connection
+      end
+    end
+  end
+
 end
